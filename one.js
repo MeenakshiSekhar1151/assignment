2ef9b9740aa2279e5053d8d5ffc649781de32852
Y.Meenakshi <meenu.yasalapu@gmail.com>
2021-03-25 12:24:11 +0530

    message

--- one.js
+++ one.js
@@ -1,3 +1,10 @@
-let num=31;
-num%3==0 ? console.log("multiple") : console.log("not multiple");
+//let num=31;
+//num%3==0 ? console.log("multiple") : console.log("not multiple");
+//let x=14;
+
+if(x%7==0){
+    console.log("divisible by true");
+}else{
+    console.log("not divisible ");
+}
 
